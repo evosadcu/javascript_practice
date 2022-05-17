35aa775a7e19b065b444c5ebbeb05b40837eda72
evosadcu <berrin.m.bm@gmail.com>
2022-05-17 20:32:03 +0200

    Added BeginnerFunctions Folder

---
+++ prompt/index.js
@@ -0,0 +1,10 @@
+var enterValue = prompt ("Enter a value: ")
+enterValue = parseInt (enterValue)
+
+var enterSecondValue = prompt ("Enter second value: ")
+enterSecondValue = parseInt (enterSecondValue)
+
+var enterThirdValue = prompt ("Enter third value: ")
+enterThirdValue = parseInt (enterThirdValue)
+
+console.log (enterValue + enterSecondValue + enterThirdValue)
